fb587a57b2b65aa75e072a74c42b199b09095c79
Lucas Praciano <luskcct@gmail.com>
2022-05-04 05:31:50 -0300

    Implantado Buscar na table users

--- modules/users/user.py
+++ modules/users/user.py
@@ -3,6 +3,7 @@ from flask import Blueprint, render_template, request
 
 # Created Imports
 from modules.users.controllers.user_controller import check_login_password, get_all_users
+from resources.py.token.token_manager import token_authentication
 
 user_blueprint = Blueprint('user', __name__,
                            template_folder='templates',
@@ -10,6 +11,7 @@ user_blueprint = Blueprint('user', __name__,
 
 
 @user_blueprint.route('/', methods=['GET', 'POST'])
+@token_authentication
 def user():
     if request.method == 'GET':
         return get_all_users()
@@ -26,6 +28,7 @@ def user_authentication():
 
 
 @user_blueprint.route('/manager', methods=['GET'])
+@token_authentication
 def user_manager():
     if request.method == 'GET':
         return render_template('user_manager.html'), 200
