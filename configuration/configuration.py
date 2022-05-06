90946500cf9fa32440a456517b1083b43943ac5e
Lucas Praciano <luskcct@gmail.com>
2022-05-06 10:16:01 -0300

    Adiocinado arquivo .env

--- configuration/configuration.py
+++ configuration/configuration.py
@@ -1,23 +1,26 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 class Configuration:
     HOST = '0.0.0.0'
     PORT = 5001
     TEMPLATES_AUTO_RELOAD = True
-    SECRET_KEY = 'eb_54okkma5t_=z3z%tazp!xutgfif+3b76(apf-acr6m@71#$'
+    SECRET_KEY = os.getenv('SECRET_KEY')
     ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-    TOKEN_NAME = 'new_app'
+    TOKEN_NAME = 'new_app'  # Name of Cookie to set to the browser
     TIME_EXP_TOKEN = 30  # Time in minutes of JWT token
     LIMIT_EXP_TOKEN = 1  # Time in minutes of JWT token to expire
 
 
 class DevelopmentConfig(Configuration):
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Vish_123123@127.0.0.1:5432/desafio042022'
+    SQLALCHEMY_DATABASE_URI = os.getenv('DEV_SQLALCHEMY_DATABASE_URI')
 
 
 class ProductionConfig(Configuration):
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Vish_123123@db:5432/desafio042022'
+    SQLALCHEMY_DATABASE_URI = os.getenv('PROD_SQLALCHEMY_DATABASE_URI')
 
 
 app_configuration = {
