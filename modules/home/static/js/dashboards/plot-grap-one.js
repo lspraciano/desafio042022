3694a03168cc5668ea8f6e5fdc5c684202145025
Lucas Praciano <luskcct@gmail.com>
2022-06-08 02:11:36 -0300

    Atualizada cores doss gráficos do dashboard

--- modules/home/static/js/dashboards/plot-grap-one.js
+++ modules/home/static/js/dashboards/plot-grap-one.js
@@ -1,18 +1,15 @@
 export const plotGrapOne = (labels, values) => {
     const canvasGraphOne = document.getElementById('graph-one').getContext('2d');
 
-    let gradient = canvasGraphOne.createLinearGradient(0, 0, 0, 400);
-    gradient.addColorStop(0, "rgba(82,83,163,0.7)");
-    gradient.addColorStop(1, "rgba(82,83,163,0.7)");
-
     const data = {
         labels: labels,
         datasets: [{
             label: 'Total de Transações / Dia',
             data: values,
-            fill: false,
-            borderColor: 'rgb(75, 192, 192)',
-            tension: 0.1
+            fill: true,
+            backgroundColor: 'rgba(82,83,163,0.25)',
+            borderColor: 'rgba(82,83,163,0.7)',
+            tension: 0.1,
         }]
     }
 
@@ -28,6 +25,16 @@ export const plotGrapOne = (labels, values) => {
                 }
             }
         },
+        animation: {
+            x: {
+                duration: 3000,
+                from: 0
+            },
+            y: {
+                duration: 2000,
+                from: 500
+            }
+        }
     };
 
     const myChart = new Chart(canvasGraphOne, config);
