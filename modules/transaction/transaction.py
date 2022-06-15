020fcc691bfa64355235c26c14e7f0ec8366cfd2
Lucas Praciano <luskcct@gmail.com>
2022-06-15 05:17:31 -0300

    Reformatando código

--- modules/transaction/transaction.py
+++ modules/transaction/transaction.py
@@ -32,67 +32,37 @@ def transactions():
         return save_transactions_list(request.json)
 
 
-@transaction_blueprint.route(
-    '/import',
-    methods=[
-        'GET',
-    ],
-)
+@transaction_blueprint.route('/import', methods=['GET'])
 @token_authentication
 def import_csv():
     return render_template('import_transaction.html')
 
 
-@transaction_blueprint.route(
-    '/log',
-    methods=[
-        'GET',
-    ],
-)
+@transaction_blueprint.route('/log', methods=['GET'])
 @token_authentication
 def audit_transaction():
     return render_template('transaction_log.html')
 
 
-@transaction_blueprint.route(
-    '/log/get-log',
-    methods=[
-        'GET',
-    ],
-)
+@transaction_blueprint.route('/log/get-log', methods=['GET'])
 @token_authentication
 def audit_transaction_data():
     return get_all_logs(), 200
 
 
-@transaction_blueprint.route(
-    '/suspect/report',
-    methods=[
-        'GET',
-    ],
-)
+@transaction_blueprint.route('/suspect/report', methods=['GET'])
 @token_authentication
 def report_transactions_suspect():
     return render_template('suspects_transaction.html')
 
 
-@transaction_blueprint.route(
-    '/suspect',
-    methods=[
-        'GET',
-    ],
-)
+@transaction_blueprint.route('/suspect', methods=['GET'])
 @token_authentication
 def transactions_suspect():
     return get_suspects_transactions_report(request)
 
 
-@transaction_blueprint.route(
-    '/report',
-    methods=[
-        'GET',
-    ],
-)
+@transaction_blueprint.route('/report', methods=['GET'])
 @token_authentication
 def transactions_report():
     return get_transactions_report()
